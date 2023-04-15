43bc23ff15a4d2890a1508b48321838a80cb5533
Iryna Hudau <Airan.inn1@gmail.com>
2023-04-14 20:57:17 -0400

    Update listfiles.php

--- listfiles.php
+++ listfiles.php
@@ -5,7 +5,7 @@ while (false !== ($entry = readdir($handle))) {
     if ($entry != "." && $entry != "..") {
 
         echo "$entry\n";
-        echo file_get_contents( $entry);
+        echo "file_get_contents( $entry)";
         echo "\n========\n"
     }
 }
